b644b4513da3a5f23b587ccb93a349ed0f89cc80
frthid <zvezdochets02@gmail.com>
2023-03-09 19:53:50 +0300

    add props

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './MyPosts.module.css';
+import Post from './Post/Post';
+
+const MyPosts = () => {
+    return (
+        <div>
+            My post
+            <div>
+                New post
+                <textarea></textarea>
+                <button>Add post</button>
+                <button>Remove</button>
+            </div>
+            <div className={classes.posts}>
+               <Post message='Hi' />
+               <Post message='I am chehenets' />  
+            </div>
+        </div>
+    );
+}
+
+export default MyPosts;
