50a723e9ace362857c4bf51635e1787acfe83758
frthid <zvezdochets02@gmail.com>
2023-03-09 15:50:51 +0300

    add css-module

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const  App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
-      <nav className='nav'>
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav> 
-      <div className='content'>
-        <div>
-          <img src='https://i.imgur.com/UehVKrn.png' alt=''/> 
-        </div>
-        <div>
-          ava + info
-        </div>
-        <div>
-          my post
-          <div>
-            new post
-          </div>
-          <div>
-            post 1
-          </div>
-          <div>
-            post 2
-          </div>
-        </div>
-      </div>
+      <Navbar /> 
+      <Profile />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
-          <img src ='https://p1.zoon.ru/e/a/57f5ee27fd037d5b428b4576_5c0653eae91f4.jpg' alt=''/>
-      </header>
+        <header className={classes.header}>
+            <img src ='https://p1.zoon.ru/e/a/57f5ee27fd037d5b428b4576_5c0653eae91f4.jpg' alt=''/>
+        </header>
     );
 }
 
