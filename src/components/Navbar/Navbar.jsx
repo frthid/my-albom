dfc15845a171610f0dec01934d6d02128ac36eee
frthid <zvezdochets02@gmail.com>
2023-03-11 15:16:35 +0300

    Added NavLink and SPA complite

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-      <nav className={classes.nav}>
-        <div className={classes.item}>
-          <a href='/profile'>Profile</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/dialogs'>Messages</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/news'>News</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/music'>Music</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/settings'>Settings</a>
-        </div>
-      </nav>
+        <nav className={classes.nav}>
+            <div>
+                <NavLink to='/profile' className={navData => navData.isActive ? classes.active : classes.item} >Profile</NavLink>
+            </div>
+            <div>
+                <NavLink to='/dialogs' className={navData => navData.isActive ? classes.active : classes.item}>Messages</NavLink>
+            </div>
+            <div>
+                <NavLink to='/news' className={navData => navData.isActive ? classes.active : classes.item}>News</NavLink>
+            </div>
+            <div>
+                <NavLink to='/music' className={navData => navData.isActive ? classes.active : classes.item}>Music</NavLink>
+            </div>
+            <div>
+                <NavLink to='/settings' className={navData => navData.isActive ? classes.active : classes.item}>Settings</NavLink>
+            </div>
+        </nav>
     );
 }
 
