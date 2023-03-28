d903fda74192c70fc46fd211c99e1aaeb1a519fc
frthid <zvezdochets02@gmail.com>
2023-03-28 23:20:58 +0300

    added actionCreator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-// import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
-
 
 export default function MyPosts(props) {
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch({type: 'ADD-POST'});
+        props.dispatch(addPostActionCreator());
     };
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text});
+        props.dispatch(updateNewPostTextActionCreator(text));
     };
 
     return (
